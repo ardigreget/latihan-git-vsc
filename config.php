4e3524bf2a6ef3ad2527470d6e4eeefc7c64d81d
ototiccom <157091409+ototiccom@users.noreply.github.com>
2024-09-30 06:40:39 +0700

    Pesan commit Anda

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Konfigurasi Database
 $host = "localhost";      // Host database
-$db_user = "sql_akses_wallmv";        // Username database
-$db_password = "5531dc927f2ce";        // Password database
-$db_name = "sql_akses_wallmv"; // Nama database
+$db_user = "u934654818_akses";        // Username database
+$db_password = "Sqn^y^8r";        // Password database
+$db_name = "u934654818_akses"; // Nama database
 
 // Membuat koneksi
 $conn = new mysqli($host, $db_user, $db_password, $db_name);
