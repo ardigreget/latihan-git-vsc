d1892eed819761f2a7ce294d28d54d4866d332c3
ototiccom <157091409+ototiccom@users.noreply.github.com>
2024-09-30 10:39:03 +0700

    a

--- e3/background.js
+++ e3/background.js
@@ -1,15 +1,19 @@
-chrome.action.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener(function() {
+    // Panggil API untuk mengecek status login
     fetch('http://akses.papindo.id/b3/api.php', { credentials: 'include' })
     .then(response => response.json())
     .then(data => {
         if (data.status === 'error') {
-            // Jika tidak login, buka halaman login di tab baru
+            // Jika belum login, buka halaman login di tab baru
             chrome.tabs.create({ url: 'http://akses.papindo.id/b3/login.php' });
         } else {
             // Jika sudah login, buka popup
             chrome.action.setPopup({ popup: 'popup.html' });
-            chrome.action.openPopup(); // Memastikan popup dibuka
         }
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+        // Jika terjadi error, buka halaman login sebagai fallback
+        chrome.tabs.create({ url: 'http://akses.papindo.id/b3/login.php' });
+    });
 });
